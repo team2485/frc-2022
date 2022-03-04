96cbcf06ea7c29cef1db51d6eb8d3766e4939fce
Mark <mrifkin2022@francisparker.org>
2022-03-03 19:17:21 -0800

    feeder working

--- src/main/java/frc/robot/subsystems/cargoHandling/indexing/HighIndexer.java
+++ src/main/java/frc/robot/subsystems/cargoHandling/indexing/HighIndexer.java
@@ -5,6 +5,7 @@ import static frc.robot.Constants.IndexerConstants.*;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import io.github.oblarg.oblog.Loggable;
@@ -12,6 +13,9 @@ import io.github.oblarg.oblog.annotations.Config;
 
 public class HighIndexer extends SubsystemBase implements Loggable {
   private CANSparkMax m_spark = new CANSparkMax(kHighIndexerSparkPort, MotorType.kBrushless);
+
+  private Servo m_servo = new Servo(0);
+  private double m_servoPositionSetpoint = 0;
 
   public HighIndexer() {
     m_spark.enableVoltageCompensation(Constants.kNominalVoltage);
@@ -23,5 +27,14 @@ public class HighIndexer extends SubsystemBase implements Loggable {
   @Config.NumberSlider(name = "Set percent output high", tabName = "Indexing")
   public void setPercentOutput(double percentOutput) {
     m_spark.set(percentOutput);
+  }
+
+  @Config(name = "Set servo", tabName = "Indexing")
+  public void setServo(double position) {
+    m_servoPositionSetpoint = position;
+  }
+
+  public void periodic() {
+    m_servo.set(m_servoPositionSetpoint);
   }
 }
