f30e7fc0d67f023aa3b0025cda96a58cd4457a4e
Jack Douglas <jackdouglas2@me.com>
2023-01-21 13:34:37 -0800

    continue pose estimation

--- src/main/java/frc/robot/commands/vision/PoseEstimation.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.commands.vision;
-
-import edu.wpi.first.wpilibj2.command.CommandBase;
-
-public class PoseEstimation extends CommandBase {
-
-}
--- src/main/java/frc/util/DrivetrainPoseEstimator.java
+++ src/main/java/frc/util/DrivetrainPoseEstimator.java
@@ -1,5 +1,47 @@
 package frc.util;
 
-public class DrivetrainPoseEstimator {
+import org.photonvision.PhotonCamera;
 
+import com.ctre.phoenix.sensors.WPI_Pigeon2;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.numbers.N5;
+import edu.wpi.first.math.util.Units;
+import frc.robot.Constants.Swerve;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.subsystems.drive.Drivetrain;
+
+public class DrivetrainPoseEstimator {
+  // sensors for pose estimation
+  private final WPI_Pigeon2 gyro = new WPI_Pigeon2(Swerve.pigeonID);
+  private PhotonCamera camera = new PhotonCamera(VisionConstants.kCameraName);
+  private Drivetrain drivetrain = new Drivetrain();
+
+  // kalman filter config
+  Matrix<N5, N1> stateStdDevs = VecBuilder.fill(0.05, 0.05, Units.degreesToRadians(5), 0.05, 0.05);
+  Matrix<N3, N1> localMeasurementStdDevs = VecBuilder.fill(0.01, 0.01, Units.degreesToRadians(0.1));
+  Matrix<N3, N1> visionMeasurementStdDevs = VecBuilder.fill(0.01, 0.01, Units.degreesToRadians(0.1));
+
+  public final SwerveDrivePoseEstimator m_poseEstimator;
+
+  public DrivetrainPoseEstimator(double lestDist, double rightDist) {
+    m_poseEstimator = new SwerveDrivePoseEstimator(Swerve.swerveKinematics, gyro.getRotation2d(),
+        drivetrain.getModulePositions(), new Pose2d());
+  }
+
+  // perform periodic pose estimation tasks
+  public void update(SwerveModulePosition[] modulePositions) {
+    m_poseEstimator.update(gyro.getRotation2d(), modulePositions);
+
+    var result = camera.getLatestResult();
+    if (result.hasTargets()) {
+      var camToTargetTrans = result.getBestTarget().getCameraToTarget();
+    }
+  }
 }
